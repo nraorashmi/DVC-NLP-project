3c32922f56b6023646c607bf0528c634ed6f297a
Rashmi <nraorashmi@gmail.com>
2022-07-05 11:18:31 +0530

    comments added

--- docs/data_prep.md
+++ docs/data_prep.md
@@ -9,7 +9,7 @@ data.xml
 ```
 - We are choosing only three tags in the xml data - 1. row Id, 2. title and body 3. Tags (Stackoverflow tags specific to python)
 
-|Tags|feaures names|
+|Tags|features names|
 |-|-|
 |row Id|row ID|
 |title and body|text|
--- src/stage_01_prepare_data.py
+++ src/stage_01_prepare_data.py
@@ -1,7 +1,8 @@
 import argparse
 import os
 import logging
-from src.utils import read_yaml, create_directories, process_posts
+from src.utils.common import read_yaml, create_directories, process_posts
+from src.utils.data_mgmt import process_posts
 import random
 
 
