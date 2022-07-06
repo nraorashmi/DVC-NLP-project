dc271696bf82c543d8c1c5a2d709589ab243f541
Rashmi <nraorashmi@gmail.com>
2022-07-06 12:45:37 +0530

    added username

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "REPO_NAME"
+REPO_NAME = "DVC-NLP-project"
 AUTHOR_USER_NAME = "nraorashmi"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = []
